fcf903b6be8b0cab3b8c5f7fc5a6cbc0e006a4a9
Madhuri <madhurisalunkhe777@gmail.com>
2025-12-05 00:53:18 +0530

    Fix path

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,4 +1,6 @@
-import { createUser } from "../services/userService";
+
+import { createUser } from "../Services/userService";
+
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
